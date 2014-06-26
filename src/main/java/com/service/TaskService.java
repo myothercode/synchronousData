f8e92c6715f86f36d555ce04eabb57985489a3d2
caixu23 <caixu23@gmail.com>
2014-06-26 17:38:45 +0800

    增加定时任务的文件

---
+++ src/main/java/com/service/TaskService.java
@@ -0,0 +1,11 @@
+package com.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by chace.cai on 2014/6/26.
+ */
+public interface TaskService {
+    /**为第二步查询数据，从数据库*/
+    void getDateFromForStepTwo();
+}
